b42e9f9c56b41e5c4bfc21a5fccb192f1c7bca05
physcrowley <crowlda@ecolecatholique.ca>
2021-01-21 16:19:38 -0500

    comments and minor edits to transitions

--- transitions/src/main/java/edu/djc/transitions/TransitionsController.java
+++ transitions/src/main/java/edu/djc/transitions/TransitionsController.java
@@ -15,13 +15,24 @@ import javafx.util.Duration;
 
 public class TransitionsController {
 
-    private boolean clicked = false;
+	/* Class fields that can be passed to the class methods 
+	 * without passing parameters. These fields are named in
+	 * the FXML file (the variable name needs to be the same here).
+	 * 
+	 * Having class fields is necessary since no parameters can 
+	 * be passed from FXML method calls.
+	 */
 	
 	@FXML
     private Rectangle r;
     
     @FXML
     private Button run;
+    
+    
+    /*
+     * Methods referenced in the linked FXML file
+     */
     
     @FXML
     private void toHome() throws IOException {
@@ -30,38 +41,42 @@ public class TransitionsController {
     
     @FXML
     private void runAnimation() {
-    	if (clicked) run.setText("deactivated");
+    	/* This prevents launching the infinite animation more than 
+		 * once, which produces weird results.
+		 */
+    	run.setDisable(true);
     	
-    	else
-    	{
-    		clicked = true;
-    		
-	    	r.setFill(Color.PINK);
-	    	
-	    	TranslateTransition translate = 
-	        new TranslateTransition(Duration.millis(850)); 
-	        translate.setToX(400); 
-	        translate.setToY(400); 
-	       
-	        //Filling transition
-	        FillTransition fill = new FillTransition(Duration.millis(425)); 
-	        fill.setToValue(Color.YELLOW); 
-	       
-	        //Setting rotation time
-	        RotateTransition rotate = new RotateTransition(Duration.millis(850)); 
-	        rotate.setToAngle(350); 
-	             
-	        //Scaling transition
-	        ScaleTransition scale = new ScaleTransition(Duration.millis(850)); 
-	        scale.setToX(0.2); 
-	        scale.setToY(0.2); 
-	             
-	        //Setting parallel transition
-	        ParallelTransition transition = new ParallelTransition(r, 
-	        translate, fill, rotate, scale); 
-	        transition.setCycleCount(Timeline.INDEFINITE);
-	        transition.setAutoReverse(true); 
-	        transition.play();
-    	}
+    	
+		/*
+		 * This animation is adapted from the non-FXML example on
+		 * this site: http://mrbool.com/a-complete-guide-to-javafx/33010
+		 */
+		r.setFill(Color.PINK);
+    	
+    	TranslateTransition translate = 
+        new TranslateTransition(Duration.millis(850)); 
+        translate.setToX(400); 
+        translate.setToY(400); 
+       
+        //Filling transition
+        FillTransition fill = new FillTransition(Duration.millis(425)); 
+        fill.setToValue(Color.YELLOW); 
+       
+        //Setting rotation time
+        RotateTransition rotate = new RotateTransition(Duration.millis(850)); 
+        rotate.setToAngle(350); 
+             
+        //Scaling transition
+        ScaleTransition scale = new ScaleTransition(Duration.millis(850)); 
+        scale.setToX(0.2); 
+        scale.setToY(0.2); 
+             
+        //Setting parallel transition
+        ParallelTransition transition = new ParallelTransition(r, 
+        translate, fill, rotate, scale); 
+        transition.setCycleCount(Timeline.INDEFINITE);
+        transition.setAutoReverse(true); 
+        transition.play();
+
     }
 }
