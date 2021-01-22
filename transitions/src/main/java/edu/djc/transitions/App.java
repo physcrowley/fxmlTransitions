ed00d7e6a91d7fc2a5fa65ce38420d221794a222
physcrowley <crowlda@ecolecatholique.ca>
2021-01-21 23:34:30 -0500

    removed unused import

--- transitions/src/main/java/edu/djc/transitions/App.java
+++ transitions/src/main/java/edu/djc/transitions/App.java
@@ -2,10 +2,8 @@ package edu.djc.transitions;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 /**
