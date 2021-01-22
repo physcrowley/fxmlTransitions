776c3ee889f93d5a3a80e8abdbc9a7361c28fd42
physcrowley <crowlda@ecolecatholique.ca>
2021-01-21 22:44:03 -0500

    removed home page and simplified related code

--- transitions/src/main/java/edu/djc/transitions/App.java
+++ transitions/src/main/java/edu/djc/transitions/App.java
@@ -11,27 +11,19 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
-
-    private static Scene scene;
-
+public class App extends Application 
+{
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"), 600, 600);
+    public void start(Stage stage) throws IOException 
+    {
+    	FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("transitions.fxml"));
+    	var scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch();
     }
 
